fba7944edf86c4e240d50cfdd9524430da77d88b
Halsey Svetz <halseycheese@gmail.com>
2025-10-20 10:39:05 -0600

    still troubleshooting image fetching from cloudinary

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // … other config …
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        // optionally port: '', pathname: '/dghgutvux/image/upload/**'
+        pathname: '/dghgutvux/image/upload/**',
+      },
+    ],
+    // optional: domains: ['res.cloudinary.com'], for older Next versions
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
