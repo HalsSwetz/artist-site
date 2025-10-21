8e90af9f12013e259b6f8691ee74045da0db7bd3
Halsey Svetz <halseycheese@gmail.com>
2025-10-21 16:00:08 -0600

    having vercel caching issues, trying to debug

--- cloudinary.ts
+++ cloudinary.ts
@@ -3,9 +3,9 @@ const CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || "dghgutvux";
 export const cld = {
   full: (publicId: string, width: number, cropMode: string = "fit", quality: number = 85): string => {
     const cleanId = publicId.replace(/^\/+/, '');
-    const url = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/w_${width},c_${cropMode},f_auto,q_${quality}/${cleanId}`;
+    // Add version param to bust cache
+    const url = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/w_${width},c_${cropMode},f_auto,q_${quality}/${cleanId}?v=2`;
     
-    // Debug logging
     console.log('Cloudinary URL:', url);
     
     return url;
